6b79701807f4b497b1324879667bb61b231aea0e
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-12-01 15:12:31 +0530

    added chapters and transcripts

--- pages/episods/[id]/index.tsx
+++ pages/episods/[id]/index.tsx
@@ -2,16 +2,19 @@ import { useRouter } from "next/router";
 import processedEpisods from "../../../public/processedEpisods.json";
 import Head from "next/head";
 import Header from "../../../components/Header";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, memo } from "react";
 import ReactPlayer from "react-player/youtube";
 import TabsTest from "../../../components/TabsTest"
-import chapters from '../../../public/402Chapters.json'
-import Transcripts from "../../../public/402Transcripts.json"
-
+import chapters402 from '../../../public/402Chapters.json'
+import Transcripts402 from "../../../public/402Transcripts.json"
+import chapters401 from '../../../public/401Chapters.json'
+import Transcripts401 from "../../../public/401Transcripts.json"
 
 export default function Episods() {
 
   const [activeTabIndex, setActiveTabIndex] = useState<number>(0);
+  const [data, setData] = useState<{ chapters: any[]; transcripts: any[] } | null>(null);
+
   
   const router = useRouter();
   const { id } = router.query;
@@ -20,6 +23,65 @@ export default function Episods() {
   const episode = processedEpisods.find((ep) => ep.url === id);
 
   const playerRef = useRef<ReactPlayer | null>(null);
+
+  useEffect(() => {
+    // https://chat.openai.com/share/9ab62daf-502d-402d-9693-c659466dcaff
+    const fetchData = async () => {
+      try {
+        let chapters, transcripts;
+    
+        switch (id) {
+          case '402':
+            const chaptersModule402 = await import('../../../public/402Chapters.json');
+            const transcriptsModule402 = await import('../../../public/402Transcripts.json');
+            chapters = chaptersModule402.default; // Extract the array from the module
+            transcripts = transcriptsModule402.default; // Extract the array from the module
+            break;
+          case '401':
+            const chaptersModule401 = await import('../../../public/401Chapters.json');
+            const transcriptsModule401 = await import('../../../public/401Transcripts.json');
+            chapters = chaptersModule401.default; // Extract the array from the module
+            transcripts = transcriptsModule401.default; // Extract the array from the module
+            break;
+
+          case '400':
+              const chaptersModule400 = await import('../../../public/400Chapters.json');
+              const transcriptsModule400 = await import('../../../public/400Transcripts.json');
+              chapters = chaptersModule400.default; // Extract the array from the module
+              transcripts = transcriptsModule400.default; // Extract the array from the module
+              break;
+          case '399':
+              const chaptersModule399 = await import('../../../public/399Chapters.json');
+              const transcriptsModule399 = await import('../../../public/399Transcripts.json');
+              chapters = chaptersModule399.default; // Extract the array from the module
+              transcripts = transcriptsModule399.default; // Extract the array from the module
+              break;
+          
+          default:
+            const chaptersModuleOther = await import('../../../public/402Chapters.json');
+            const transcriptsModuleOther = await import('../../../public/402Transcripts.json');
+            chapters = chaptersModuleOther.default; // Extract the array from the module
+            transcripts = transcriptsModuleOther.default; // Extract the array from the module
+            break;
+        }
+    
+        console.log('Fetched data:', { chapters, transcripts });
+        setData({ chapters, transcripts });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle errors appropriately, e.g., redirect to an error page
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  if (!data) {
+    // Loading state or handle other cases where data is not available
+    return <div>Loading...</div>;
+  }
 
   // Check if the episode is found
   if (!episode) {
@@ -92,8 +154,8 @@ export default function Episods() {
 
                 <div className="content">
                   {activeTabIndex === 0 && (
-                    <div className="mt-10 sm:ml-10">
-                      {chapters.map((chapter, index) => (
+                    <div className="mt-10 sm:ml-10" style={{ maxHeight: '600px', overflowY: 'auto' }}>
+                      {data.chapters.map((chapter, index) => (
                         <div key={index}>
                           <h1 className="text-lg font-semibold sm:text-xl lg:text-xl">{chapter.chapter_title}</h1>
                           <div className="text-color-secondary flex items-center gap-x-2">
@@ -114,11 +176,11 @@ export default function Episods() {
                   )}
 
                   {activeTabIndex === 1 && (
-                    <div className="mt-10 sm:ml-10">
-                      {Transcripts.map((transcript, index) => (
+                    <div className="mt-10 sm:ml-10" style={{ maxHeight: '600px', overflowY: 'auto' }}>
+                      {data.transcripts.map((transcript, index) => (
                         <div key={index}>
                           <div className="text-color-secondary text-base flex items-center gap-x-2">
-                          <p className="italic">{transcript.speaker}</p>
+                            <p className="italic">{transcript.speaker}</p>
                             <button onClick={() => playerRef.current?.seekTo(parseTimeString(transcript.start_time))}>
                               {transcript.start_time}
                             </button>
@@ -129,7 +191,6 @@ export default function Episods() {
                           <a className="mt-2 text-lg font-semibold flex items-start gap-x-2 mb-5">
                             {transcript.text}
                           </a>
-
                         </div>
                       ))}
                     </div>
