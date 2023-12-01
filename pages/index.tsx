43b67b680e93578796244cc70bbacaa80e9e5e75
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-12-01 18:26:18 +0530

    added my twitter url

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
           {/* <Badge text={"Try our GenAI solution for Contact Center"} /> */}
 
           <a
-          href="/"
+          href="https://twitter.com/rahul_jalindar"
           target="_blank"
           rel="noreferrer"
           className="mx-auto flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
